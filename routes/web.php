473ae7b9748d41eba899a0bd3b7c0c0cb9c58ea4
0522arai <0522arai.mail@gmail.com>
2023-08-06 15:48:39 +0900

    投稿の一覧表示

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/', function(){
+    return view('posts.index');
+});
+
+Route::get('/', [PostController::class, 'index']);
